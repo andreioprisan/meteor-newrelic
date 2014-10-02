b0f349949e20cce2ef55d5f871fbe62d16cf9c8b
Andrei Oprisan <andrei@oprisan.com>
2014-10-01 23:04:04 -0400

    v1.0.2

--- package.js
+++ package.js
@@ -1,11 +1,19 @@
 Package.describe({
-  summary: "meteor sql bindings for server-side calls"
-});
+  summary: "meteor sql bindings for server-side calls",
+  "name": "mycartio:newrelic",
+  "version": "1.0.2",
+  "author": "Andrei Oprisan (http://andrei.oprisan.com)",
+  "homepage": "https://github.com/mycartio/newrelic",
+  "git": "https://github.com/mycartio/newrelic.git",
+}});
 
-Npm.depends({ "newrelic": "1.x.x" });
+Npm.depends({ "newrelic": "1.11.3" });
 
 Package.on_use(function(api) {
-  if (api.export) api.export('newrelic', 'server');	
-  api.add_files('newrelic_npm.js', 'server');
+  api.versionsFrom("METEOR@0.9.0");
+  if (api.export) 
+  	api.export('newrelic', 'server');	
+
+  api.add_files('lib/server.js', 'server');
 });
 
